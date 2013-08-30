ba210a3371a62160fb96c476f4f55975fce1636c
Andrew De Ponte <cyphactor@gmail.com>
2013-08-30 06:23:32 -0700

    Disabled protect forgery on pairing session create
    I did this because it is an API call and there is no view to load the CSRF
    before hand.

--- app/controllers/api/v1/pairing_sessions_controller.rb
+++ app/controllers/api/v1/pairing_sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PairingSessionsController < ApplicationController
+  protect_from_forgery :except => [:create]
   def create
     InviteMailer.invite(params[:system_username], params[:pairing_users], params[:port_number]).deliver
     render json: {}
