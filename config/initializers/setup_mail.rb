de1143ec48d429a2c0f10b1a45c52aa07c90fe64
Andrew De Ponte <cyphactor@gmail.com>
2013-08-30 05:20:24 -0700

    Added mailer setup.
    I did this so that I could actually use the mailer for password resets and
    eventually notifications.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => "noreply@tmux.me",
-  :password             => "porkbork",
+  :user_name            => "andrew.deponte@tmux.me",
+  :password             => "gj7OsrA0hCkQ",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
