6f619d45f3a32a4daab3f4161984fae78f478736
Andrew De Ponte <cyphactor@gmail.com>
2013-08-30 05:40:07 -0700

    Fixed a route I accidently broke before.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Tmuxme::Application.routes.draw do
   resources :users, :only => [:new, :create]
   resources :sessions, :only => [:new, :create, :destroy]
   resources :password_resets, :only => [:new, :create, :edit, :update]
-  resources :public_keys, :only => [:index]
+  resources :public_keys, :only => [:index, :new, :create, :destroy]
 
   namespace :api do
     namespace :v1 do
