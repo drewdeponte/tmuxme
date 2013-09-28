7eac0927fd3db43c9b6ffbc8db97cd59fce2252a
Andrew De Ponte <cyphactor@gmail.com>
2013-09-28 12:26:54 -0700

    Moved smtp email setup to production.rb
    I did this because we only want to setup the SMTP delivery method in
    production. In the test environment we use the built in :test delivery
    mechanism and in development we use the built in :file delivery mechanism.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,3 +81,14 @@ Tmuxme::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
+
+EMAIL_CONFIG = YAML::load_file(File.join(File.dirname(__FILE__), '..', 'email.yml'))[::Rails.env]
+
+ActionMailer::Base.smtp_settings = {
+  :address              => EMAIL_CONFIG['address'],
+  :port                 => EMAIL_CONFIG['port'],
+  :user_name            => EMAIL_CONFIG['user_name'],
+  :password             => EMAIL_CONFIG['password'],
+  :authentication       => EMAIL_CONFIG['authentication'],
+  :enable_starttls_auto => EMAIL_CONFIG['enable_starttls_auto']
+}
