b47d617830a31e6e035dc421af03624002c72fca
Andrew De Ponte <cyphactor@gmail.com>
2013-09-28 11:58:14 -0700

    Made the dev environment use file mail method.
    I did this so that in development we don't need access to the production SMTP
    credentials and configuration. Note, I explicitly set the production
    environment to use the smtp delivery method.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Tmuxme::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => "tmux.me" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
