6c7159591c113efe32af876d0b6cd4d6a9c0b915
Andrew De Ponte <cyphactor@gmail.com>
2013-09-28 23:37:49 -0700

    Moved secret token to a config file.
    I did this so that I could easily have the puppet configurations manage a
    separate production secret_token.yml config with a separate secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tmuxme::Application.config.secret_key_base = '09bfc2aaf733c4c43103184544c7f8dfe2d9fb6e433306ddf641d549165aa2e12a4c88c830752b6677e2543120d8368088c26aa8a3368eb5c1817899c7d82bf5'
+SECRET_TOKEN_CONFIG = YAML::load_file(File.join(File.dirname(__FILE__), '..', 'secret_token.yml'))[::Rails.env]
+Tmuxme::Application.config.secret_key_base = SECRET_TOKEN_CONFIG['secret_key_base']
