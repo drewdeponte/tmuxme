e81d6860b51f63f7fa1d1e0b632b23dba4aac8d3
Brian Miller <brian.miller@reachlocal.com>
2015-04-22 15:23:39 -0700

    Added auth token management to view and remove github auth tokens

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Tmuxme::Application.routes.draw do
-  resources :users, :only => [:new, :create]
-  resources :sessions, :only => [:new, :create, :destroy]
-  resources :password_resets, :only => [:new, :create, :edit, :update]
-  resources :public_keys, :only => [:index, :new, :create, :destroy]
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :public_keys, only: [:index, :new, :create, :destroy]
+  resources :auth_tokens, only: [:index, :destroy]
+
+  match '/auth/:provider/callback', to: 'auth_tokens#callback', via: [:get, :post]
 
   namespace :api do
     namespace :v1 do
-      resources :pairing_sessions, :only => [:create]
-      resources :users, :constraints => { :id => /[^\/]+/ } do
-        resources :public_keys, :only => [:index]
+      resources :pairing_sessions, only: [:create]
+      resources :users, constraints: { :id => /[^\/]+/ } do
+        resources :public_keys, only: [:index]
       end
     end
   end
-
-  match '/auth/:provider/callback', to: 'auth#callback', via: [:get, :post]
 
   root to: 'landing_page#show'
 end
---
+++ features/manage_auth_tokens.feature
@@ -0,0 +1,23 @@
+Feature: Auth Token Management
+
+  In order to maange all of my auth token grants
+  As a User with granted auth tokens
+  I want to be able add or remove auth tokens
+
+  @omniauth_test
+  Scenario: A user views all registered auth tokens
+    Given I am a user
+    And I am logged in
+    And I have a github token registered
+    When I visit the auth token management page
+    Then I should see the granted github token
+
+  @omniauth_test
+  Scenario: A user removes a registered auth token
+    Given I am a user
+    And I am logged in
+    And I have a github token registered
+    When I visit the auth token management page
+    And I remove the granted github token
+    Then I should see an auth token removal success message
+    And I should not see the github token
